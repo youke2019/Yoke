2342a7b4973dac001603a3c750d921b58e157b27
GuoZhi <2151259337@qq.com>
2019-07-06 05:28:39 +0800

    update apiDoc

--- Doc/apidoc/api_data.js
+++ Doc/apidoc/api_data.js
@@ -1,5 +1,30 @@
 define({ "api": [
   {
+    "type": "post",
+    "url": "/courses/search",
+    "title": "",
+    "description": "<p>根据条件搜索课程,通过post发送请求，请求参数通过json传输，json数据示例如下，所有的数据项均可为空， 即可以只有course_id的信息，所有的参数项中，course_id,course_name,teacher_name,building只能有一个，其他参数可以传递一个或者多个。</p>",
+    "name": "SearchCourseInfo",
+    "group": "courses",
+    "version": "1.0.0",
+    "header": {
+      "examples": [
+        {
+          "title": "Request-Example:",
+          "content": "{ \"course_id\": \"SE101\",\n  \"course_name\":\"计算机系统基础（1）\",\n  \"teacher_name\":\"藏斌宇\",\n  \"course_types\":[\"0\"],        // 0:非通识，1：通识\n  \"general_types\":[\"人文学科\"],   //\"社会学科\",\"人文学科\",\"自然学科\",\"工程科学与技术\",\"\",\n  \"weekdays\":[1,2],\n  \"begin_sec\":[1,2,3],\n  \"end_sec\":[3,4,5],\n  \"building\":\"东上院\",\n  \"course_credits\":[2,3,4]\n   }",
+          "type": "json"
+        },
+        {
+          "title": "Response-Example:",
+          "content": "[\n    {\n        \"course_id\": \"SE101\",\n        \"course_name\": \"计算机系统基础（1）\",\n        \"course_hours\": 80,\n        \"course_credits\": 5,\n        \"general\": false,\n        \"general_type\": \"\",\n        \"classes\": [\n            {\n                \"classname\": \"2018-2019-1-SE101-392689\",\n                \"course_id\": \"SE101\",\n                \"teacher_id\": \"11145\",\n                \"teacher_name\": \"臧斌宇\",\n                \"teachers\": \"11145/臧斌宇/教授[电子信息与电气工程学院];10886/陈榕/副教授[电子信息与电气工程学院]\",\n                \"course_participants\": 48,\n                \"classSegments\": [\n                    {\n                        \"class_sec_id\": 9250,\n                        \"classname\": \"2018-2019-1-SE101-392689\",\n                        \"classroom\": \"东上院102\",\n                        \"begin_week\": 1,\n                        \"end_week\": 16,\n                        \"begin_sec\": 7,\n                        \"end_sec\": 8,\n                        \"week\": 2,\n                        \"oddOrEven\": \"b\"\n                    },\n                    {\n                        \"class_sec_id\": 9251,\n                        \"classname\": \"2018-2019-1-SE101-392689\",\n                        \"classroom\": \"东上院102\",\n                        \"begin_week\": 1,\n                        \"end_week\": 16,\n                        \"begin_sec\": 7,\n                        \"end_sec\": 8,\n                        \"week\": 2,\n                        \"oddOrEven\": \"b\"\n                    },\n                    {\n                        \"class_sec_id\": 9252,\n                        \"classname\": \"2018-2019-1-SE101-392689\",\n                        \"classroom\": \"东上院102\",\n                        \"begin_week\": 1,\n                        \"end_week\": 16,\n                        \"begin_sec\": 3,\n                        \"end_sec\": 4,\n                        \"week\": 5,\n                        \"oddOrEven\": \"b\"\n                    },\n                    {\n                        \"class_sec_id\": 9253,\n                        \"classname\": \"2018-2019-1-SE101-392689\",\n                        \"classroom\": \"东上院102\",\n                        \"begin_week\": 1,\n                        \"end_week\": 16,\n                        \"begin_sec\": 3,\n                        \"end_sec\": 4,\n                        \"week\": 5,\n                        \"oddOrEven\": \"b\"\n                    }\n                ]\n            }\n        ]\n    }\n]",
+          "type": "json"
+        }
+      ]
+    },
+    "filename": "backend/src/main/java/com/yoke/backend/Controller/CourseController.java",
+    "groupTitle": "courses"
+  },
+  {
     "type": "get",
     "url": "/courses/update",
     "title": "",
@@ -27,7 +52,7 @@ define({ "api": [
         ]
       }
     },
-    "filename": "Code/backend/src/main/java/com/yoke/backend/Controller/CourseController.java",
+    "filename": "backend/src/main/java/com/yoke/backend/Controller/CourseController.java",
     "groupTitle": "courses"
   }
 ] });
--- Doc/apidoc/api_project.js
+++ Doc/apidoc/api_project.js
@@ -2,13 +2,13 @@ define({
   "name": "API文档",
   "version": "0.1.0",
   "description": "A basic apiDoc example",
-  "url": "localhost:8000/",
+  "url": "localhost:8000",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-07-05T06:12:22.742Z",
+    "time": "2019-07-05T21:27:42.371Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
