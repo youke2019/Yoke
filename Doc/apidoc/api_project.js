9e4fd8800ea590d42cb7bc79ef911b36e701e103
GuoZhi <2151259337@qq.com>
2019-09-02 16:08:27 +0800

    update report api

--- Doc/apidoc/api_project.js
+++ Doc/apidoc/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2019-09-02T08:02:10.335Z",
+    "time": "2019-09-02T08:07:34.943Z",
     "url": "http://apidocjs.com",
     "version": "0.17.7"
   }
